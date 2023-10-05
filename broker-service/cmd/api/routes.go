eecad4733faddbd30be884f56153ab9442d904eb
magelon6 <adizovmsu@gmail.com>
2023-10-05 13:46:43 +0300

    broker auth handler

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -23,7 +23,7 @@ func (app *Config) routes() http.Handler {
 	mux.Use(middleware.Heartbeat("/ping"))
 
 	mux.Post("/", app.Broker)
-	mux.Get("/", app.Hello)
+	mux.Post("/handle", app.HandleSubmission)
 
 	return mux
 }
